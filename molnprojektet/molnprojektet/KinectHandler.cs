34b37bc4a43ec3c2453d9d8aa6bafbf289bcb411
Emil <karlemil.lindberg@gmail.com>
2013-02-20 19:07:18 +0100

    Angle calculations

--- molnprojektet/molnprojektet/KinectHandler.cs
+++ molnprojektet/molnprojektet/KinectHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using Microsoft.Kinect;
+using Microsoft.Xna.Framework;
 
 namespace molnprojektet
 {
@@ -12,6 +13,7 @@ namespace molnprojektet
         KinectSensor sensor;
         Game1 game;
         bool running = true;
+        Skeleton currentSkeleton;
         public KinectHandler(Game1 owner)
         {
             game = owner;
@@ -62,15 +64,15 @@ namespace molnprojektet
                     skeletonFrame.CopySkeletonDataTo(skeletons);
                     skeletonFrame.Dispose();
                 }
-
-                TrackClosestSkeleton(skeletons);
-                
             }
-            
+
+            TrackClosestSkeleton(skeletons);
         }
 
         private void TrackClosestSkeleton(Skeleton[] skeletons)
         {
+            int closestID = 0;
+
             if (this.sensor != null && this.sensor.SkeletonStream != null)
             {
                 if (!this.sensor.SkeletonStream.AppChoosesSkeletons)
@@ -79,7 +81,7 @@ namespace molnprojektet
                 }
 
                 float closestDistance = 10000f; // Start with a far enough distance
-                int closestID = 0;
+                Skeleton closestSkeleton = null;
 
                 foreach (Skeleton skeleton in skeletons.Where(s => s.TrackingState != SkeletonTrackingState.NotTracked))
                 {
@@ -87,14 +89,30 @@ namespace molnprojektet
                     {
                         closestID = skeleton.TrackingId;
                         closestDistance = skeleton.Position.Z;
+                        closestSkeleton = skeleton;
                     }
                 }
 
                 if (closestID > 0)
                 {
+                    this.currentSkeleton = closestSkeleton;
                     this.sensor.SkeletonStream.ChooseSkeletons(closestID); // Track this skeleton
                 }
             }
+        }
+
+        private float calculateArmAngle(JointType startJoint1, JointType endJoint1,
+                             JointType startJoint2, JointType endJoint2)
+        {
+            Joint joint1 = currentSkeleton.Joints[startJoint1];
+            Joint joint2 = currentSkeleton.Joints[endJoint1];
+            Joint joint3 = currentSkeleton.Joints[startJoint2];
+            Joint joint4 = currentSkeleton.Joints[endJoint2];
+
+            Vector3 vector1 = new Vector3(joint2.Position.X - joint1.Position.X, joint2.Position.Y - joint1.Position.Y, joint2.Position.Z - joint1.Position.Z);
+            Vector3 vector2 = new Vector3(joint4.Position.X - joint3.Position.X, joint4.Position.Y - joint3.Position.Y, joint4.Position.Z - joint3.Position.Z);
+
+            return (float)Utils.CalculateAngle(vector1, vector2);
         }
     }
 }
