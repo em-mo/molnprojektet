194e13bf32718a4fdcdee37d243813be9a17e5fa
Emil <karlemil.lindberg@gmail.com>
2013-02-20 09:57:11 +0100

    Added kinect reading structure and skeleton selection

--- molnprojektet/molnprojektet/Game1.cs
+++ molnprojektet/molnprojektet/Game1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Kinect;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -8,6 +9,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using System.Threading;
 
 namespace molnprojektet
 {
@@ -18,6 +20,7 @@ namespace molnprojektet
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        KinectHandler kinectHandler;
 
         public Game1()
         {
@@ -34,7 +37,10 @@ namespace molnprojektet
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            
+            kinectHandler = new KinectHandler(this);
+            Thread kinectThread = new Thread(() => kinectHandler.run());
+            kinectThread.Start();
             base.Initialize();
         }
 
---
+++ molnprojektet/molnprojektet/KinectHandler.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Kinect;
+
+namespace molnprojektet
+{
+    class KinectHandler
+    {
+        KinectSensor sensor;
+        Game1 game;
+        bool running = true;
+        public KinectHandler(Game1 owner)
+        {
+            game = owner;
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the skeleton stream to receive skeleton frames
+                this.sensor.SkeletonStream.Enable();
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+        }
+
+        public void run()
+        {
+            while (running)
+            {
+                ProcessSkeletonFrame();
+            }
+        }
+
+        private void ProcessSkeletonFrame()
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = sensor.SkeletonStream.OpenNextFrame(100))
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                    skeletonFrame.Dispose();
+                }
+
+                TrackClosestSkeleton(skeletons);
+                
+            }
+            
+        }
+
+        private void TrackClosestSkeleton(Skeleton[] skeletons)
+        {
+            if (this.sensor != null && this.sensor.SkeletonStream != null)
+            {
+                if (!this.sensor.SkeletonStream.AppChoosesSkeletons)
+                {
+                    this.sensor.SkeletonStream.AppChoosesSkeletons = true; // Ensure AppChoosesSkeletons is set
+                }
+
+                float closestDistance = 10000f; // Start with a far enough distance
+                int closestID = 0;
+
+                foreach (Skeleton skeleton in skeletons.Where(s => s.TrackingState != SkeletonTrackingState.NotTracked))
+                {
+                    if (skeleton.Position.Z < closestDistance)
+                    {
+                        closestID = skeleton.TrackingId;
+                        closestDistance = skeleton.Position.Z;
+                    }
+                }
+
+                if (closestID > 0)
+                {
+                    this.sensor.SkeletonStream.ChooseSkeletons(closestID); // Track this skeleton
+                }
+            }
+        }
+    }
+}
