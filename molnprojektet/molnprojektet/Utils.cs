78b4b46b9b8d3ee8bf9ffa9a6b65128661b451a7
TheDragonsShadow <carl.razmus@outlook.com>
2013-02-20 20:00:03 +0100

    inget

---
+++ molnprojektet/molnprojektet/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using Microsoft.Xna.Framework;
+
+namespace molnprojektet
+{
+    class Utils
+    {
+        public static double CalculateAngle(Vector3 vector1, Vector3 vector2)
+        {
+            vector1.Normalize();
+            vector2.Normalize();
+            
+            Vector3 crossProduct = Vector3.Cross(vector1, vector2);
+
+            double crossProductLength = crossProduct.Z;
+
+            double dotProduct = Vector3.Dot(vector1, vector2);
+
+            double angle = Math.Atan2(crossProductLength, dotProduct);
+
+            return angle;
+        }
+    }
+}
